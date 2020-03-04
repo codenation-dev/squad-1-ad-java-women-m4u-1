9706ef9bac26c741670ab0d5fd8c4def21fd9d6c
juliarparanhos <jparanhos@estantevirtual.com.br>
2020-03-04 19:20:31 -0300

    first commit

---
+++ src/main/java/com/codenation/java/women/errorcenter/ErrorcenterApplication.java
@@ -0,0 +1,13 @@
+package com.codenation.java.women.errorcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codenation/java/women/errorcenter/ErrorcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenation.java.women.errorcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
